17ddbc46c524290ccda55869c2197d8ae04902e8
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-05-18 21:25:50 +0300

    second commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+}
 
+console.log(refs.btnStart);
+
+refs.btnStart.addEventListener('click', onColorChange);
+
+function onColorChange(event) {
+    const intervalId = setInterval(setBodyColor, 1000);
+    function setBodyColor() {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }
+    
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
