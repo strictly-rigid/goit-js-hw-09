5023052b1718ef9ec85cdec7b2d2b6985ca3a7a5
strictly-rigid <121813691+strictly-rigid@users.noreply.github.com>
2023-05-18 18:26:24 +0000

    Deploying to gh-pages from @ strictly-rigid/goit-js-hw-09@17ddbc46c524290ccda55869c2197d8ae04902e8 🚀

---
+++ 01-color-switcher.55af280f.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};console.log(t.btnStart),t.btnStart.addEventListener("click",(function(o){setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.55af280f.js.map
